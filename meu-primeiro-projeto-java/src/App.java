5981c114f95d475e47bc74faa5348e8837c6573a
brenohp <brenohpadovan@gmail.com>
2024-06-19 19:51:07 -0300

    Update App.java

--- meu-primeiro-projeto-java/src/App.java
+++ meu-primeiro-projeto-java/src/App.java
@@ -1,6 +1,6 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Testando validação");
+        System.out.println("Hello world!");
     }
 }
 
